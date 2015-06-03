02f5e14e99de93005244383ce8506e6aeeb5c347
Jacky Wang <jackyw@microsoft.com>
2015-06-03 22:25:26 +0800

    FinDataCrawler Initilization

---
+++ FinDataCrawler/FinDataCrawler/ArgumentsParser.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinDataCrawler
+{
+    class ArgumentsParser
+    {
+        public enum ArgumentToken
+        {
+            date_begin,
+            date_end,
+            directory,
+            symbol,
+            symbol_file,
+        }
+
+        public bool Success
+        {
+            get;
+            private set;
+        }
+
+        public IEnumerable<string> Arguments
+        {
+            get;
+            private set;
+        }
+
+        public IDictionary<ArgumentToken, string> ArgumentsTable
+        {
+            get;
+            private set;
+        }
+
+        public ArgumentsParser(IEnumerable<string> args)
+        {
+            this.Arguments = args;
+            this.Success = this.Parse();
+        }
+
+        public bool TryGetArgument<T>(ArgumentToken token, out T value)
+        {
+            try
+            {
+                var rawValue = this.ArgumentsTable[token];
+                value = (T)Convert.ChangeType(rawValue, typeof(T));
+                return true;
+            }
+            catch (Exception)
+            {
+                value = (T)new object();
+                return false;
+            }
+        }
+
+        private bool Parse()
+        {
+            int status = 0;
+            ArgumentToken token = ArgumentToken.symbol;
+            foreach (var argument in this.Arguments)
+            {
+                switch(status)
+                {
+                    case 0:
+                    if (argument.StartsWith("-") && Enum.TryParse(argument.Substring(1), out token))
+                    {
+                        status = 1;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                    break;
+
+                    case 1:
+                    this.ArgumentsTable.Add(token, argument);
+                    break;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ FinDataCrawler/FinDataCrawler/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinDataCrawler
+{
+    class Program
+    {
+        static string UrlPattern = "http://market.finance.sina.com.cn/downxls.php?date={0}&symbol={1}";
+
+        static void Main(string[] args)
+        {
+        }
+
+        static void DumpData(DateTime time, string symbol)
+        {
+        }
+
+        static void SaveToCache(DateTime time, string symbol)
+        {
+        }
+
+        static void LoadFromCache(DateTime time, string symbol)
+        {
+        }
+    }
+}
